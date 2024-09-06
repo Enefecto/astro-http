e49ab54d35d73937bfa631ea17bcbf66dc794ab7
Camilo Romero <camids980@gmail.com>
2024-09-06 14:26:04 -0400

    Feat: slug as any

--- src/pages/api/posts/[slug].ts
+++ src/pages/api/posts/[slug].ts
@@ -5,7 +5,7 @@ export const prerender = false;
 export const GET: APIRoute =  (async({params, request}) => {
     const {slug} = params;
 
-    const post = await getEntry("blog", slug);
+    const post = await getEntry("blog", slug as any);
 
     if (!post){
         return new Response(JSON.stringify({msg: `Post: ${slug} not found`}), {status:404, headers: {"Content-type": 'application/json'}})
